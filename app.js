c97e76a54c7f768daecbf47dc661bbe59c3814cc
rak3rman <rak3rman@gmail.com>
2020-11-02 23:08:37 -0600

    Handle events without a GPS lock

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ Description  : Initializes nodejs
 Author       : RAk3rman
 \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\*/
 
-//Packages and configuration - - - - - - - - - - - - -
+//Packages and configuration - - - - - - - - - - - - - - - - - - - - - - - - -
 //Declare packages
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -74,14 +74,17 @@ if (invalid_config) {
 } else {
     spinner.succeed('Config values have been propagated');
 }
-//End of Packages and configuration - - - - - - - - - -
+//End of Packages and configuration - - - - - - - - - - - - - - - - - - - - - -
 
-//Discord integration - - - - - - - - - - - - - - - - -
+//Discord integration - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //Create a new client instance with eris
 const bot = new eris.Client(config_storage.get('discord_bot_token'));
 
 //When the bot is connected and ready, update console
 bot.on('ready', () => {
+    //Set bot status
+    bot.editStatus("online", {name: config_storage.get('discord_bot_prefix') + "help", type: 2})
+    //Send update to console
     spinner.succeed('Connected to Discord API');
     spinner.succeed(`${chalk.blue.bold('Particlecord')} listening for ${chalk.yellow('API')} and ${chalk.cyan('Discord')} events`);
 });
@@ -92,8 +95,10 @@ bot.on('messageCreate', async (msg) => {
     let pre = config_storage.get('discord_bot_prefix');
     //Split message into components
     let parts = msg.content.split(' ');
-    if (parts[0] === pre + 'help') {
-        spinner.info(`${chalk.cyan('Discord')}: Generated response to "` + pre + `help" command`);
+    //Determine if we received a command
+    if (parts[0] === pre + 'help') { // If we receive the help command, display all Particlecord commands
+        spinner.start(`${chalk.cyan('Discord')}: Sending message to "` + pre + `help" command`);
+        //Send message to Discord channel, catch error if thrown
         try {
             await msg.channel.createMessage('**Particlecord commands**\n' +
                 '> ' + pre + 'help: Displays all Particlecord commands\n' +
@@ -102,45 +107,52 @@ bot.on('messageCreate', async (msg) => {
                 '> ' + pre + 'details <device_id>: Returns all details for a Particle device_id\n' +
                 '> ' + pre + 'alert_freq <device_id> <freq_in_min>: Sets the alert frequency in terms of minutes for a device, use 0 for verbose alerts\n' +
                 '> ' + pre + 'name <device_id> <friendly_name>: Changes the friendly name for a Particle device_id\n');
+            spinner.succeed(`${chalk.cyan('Discord')}: Sent message to "` + pre + `help" command`);
         } catch (err) {
             spinner.fail(`${chalk.cyan('Discord')}: Failed to send message`);
         }
-    } else if (parts[0]=== pre + 'status') {
-        spinner.info(`${chalk.cyan('Discord')}: Generated response to "` + pre + `status" command`);
+    } else if (parts[0]=== pre + 'status') { // If we receive the status command, return more details from the last alert
+        spinner.start(`${chalk.cyan('Discord')}: Sending message to "` + pre + `status" command`);
+        //Send message to Discord channel, catch error if thrown
         try {
             if (last_alert_device_id === '') {
                 await msg.channel.createMessage('Please wait for an alert to occur');
             } else {
                 await msg.channel.createMessage(get_status(last_alert_device_id));
             }
+            spinner.succeed(`${chalk.cyan('Discord')}: Sent message to "` + pre + `status" command`);
         } catch (err) {
             spinner.fail(`${chalk.cyan('Discord')}: Failed to send message`);
             console.log(err);
         }
-    } else if (parts[0]=== pre + 'devices') {
-        spinner.info(`${chalk.cyan('Discord')}: Generated response to "` + pre + `devices" command`);
+    } else if (parts[0]=== pre + 'devices') { // If we receive the devices command, return all known devices
+        spinner.start(`${chalk.cyan('Discord')}: Sending message to "` + pre + `devices" command`);
         let devices_payload = "";
         let all_devices = JSON.parse(devices_storage.json(null, 2));
         for (let i in all_devices){
             if (all_devices.hasOwnProperty(i)) {
-                devices_payload += '> ' + i + ' | ' + all_devices[i].device_name + ' | ' + moment(all_devices[i].last_data_update).format('MM/DD/YY [at] h:mm:ss a') + '\n';
+                devices_payload += '> ' + i + ' | ' + all_devices[i].device_name + ' | ' + moment(all_devices[i].last_data_update).format('h:mm:ss a [on] MM/DD/YY Z [UTC]') + '\n';
             }
         }
+        //Send message to Discord channel, catch error if thrown
         try {
             await msg.channel.createMessage('**Devices** (device_id | friendly_name | last_data_update)\n' + devices_payload);
+            spinner.succeed(`${chalk.cyan('Discord')}: Sent message to "` + pre + `devices" command`);
         } catch (err) {
             spinner.fail(`${chalk.cyan('Discord')}: Failed to send message`);
             console.log(err);
         }
-    } else if (parts[0] === pre + 'details') {
-        spinner.info(`${chalk.cyan('Discord')}: Generated response to "` + pre + `details" command`);
+    } else if (parts[0] === pre + 'details') { // If we receive the details command, return all details for a Particle device_id
+        spinner.start(`${chalk.cyan('Discord')}: Sending message to "` + pre + `details" command`);
+        //Send message to Discord channel, catch error if thrown
         try {
             await msg.channel.createMessage(get_status(parts[1]));
+            spinner.succeed(`${chalk.cyan('Discord')}: Sent message to "` + pre + `details" command`);
         } catch (err) {
             spinner.fail(`${chalk.cyan('Discord')}: Failed to send message`);
             console.log(err);
         }
-    } else if (parts[0] === pre + 'alert_freq') {
+    } else if (parts[0] === pre + 'alert_freq') { // If we receive the alert_freq command, set the alert frequency in terms of minutes for a device
         //Make sure device_id exists
         if (devices_storage.has(parts[1]) && parts[1] !== undefined) {
             let device = devices_storage.get(parts[1]);
@@ -155,6 +167,7 @@ bot.on('messageCreate', async (msg) => {
                     alert_freq_min: parts[2]
                 });
                 spinner.info(`${chalk.cyan('Discord')}: (id:` + parts[1] + `) Updated alert_freq to ` + parts[2] + ` for device due to "` + pre + `alert_freq" command`);
+                //Send message to Discord channel, catch error if thrown
                 try {
                     await msg.channel.createMessage(device.device_name + ' (' + parts[1] + ') will now send alerts at a maximum of every ' + parts[2] + ' mins');
                 } catch (err) {
@@ -163,6 +176,7 @@ bot.on('messageCreate', async (msg) => {
                 }
             } else {
                 spinner.fail(`${chalk.cyan('Discord')}: <freq_in_min> must be greater than or equal to 0, responding with error`);
+                //Send message to Discord channel, catch error if thrown
                 try {
                     await msg.channel.createMessage('ERROR: <freq_in_min> must be greater than or equal to 0, check ' + pre + 'help');
                 } catch (err) {
@@ -172,6 +186,7 @@ bot.on('messageCreate', async (msg) => {
             }
         } else {
             spinner.fail(`${chalk.cyan('Discord')}: <device_id> does not exist, responding with error`);
+            //Send message to Discord channel, catch error if thrown
             try {
                 await msg.channel.createMessage('ERROR: Particle <device_id> does not exist in database');
             } catch (err) {
@@ -179,7 +194,7 @@ bot.on('messageCreate', async (msg) => {
                 console.log(err);
             }
         }
-    } else if (parts[0] === pre + 'name') {
+    } else if (parts[0] === pre + 'name') { //If we receive the name command, change the friendly name for a Particle device_id
         //Make sure device_id exists
         if (devices_storage.has(parts[1]) && parts[1] !== undefined) {
             let device = devices_storage.get(parts[1]);
@@ -192,6 +207,7 @@ bot.on('messageCreate', async (msg) => {
                 alert_freq_min: device.alert_freq_min
             });
             spinner.info(`${chalk.cyan('Discord')}: (id:` + parts[1] + `) Updated device_name to ` + parts[2] + ` for device due to "` + pre + `name" command`);
+            //Send message to Discord channel, catch error if thrown
             try {
                 await msg.channel.createMessage(parts[2] + ' (' + parts[1] + ') was successfully renamed');
             } catch (err) {
@@ -200,6 +216,7 @@ bot.on('messageCreate', async (msg) => {
             }
         } else {
             spinner.fail(`${chalk.cyan('Discord')}: <device_id> does not exist, responding with error`);
+            //Send message to Discord channel, catch error if thrown
             try {
                 await msg.channel.createMessage('ERROR: Particle <device_id> does not exist in database');
             } catch (err) {
@@ -208,6 +225,7 @@ bot.on('messageCreate', async (msg) => {
             }
         }
     }
+    //Else, do nothing and ignore message
 });
 
 //Create a prettified status message for Discord
@@ -225,7 +243,7 @@ function get_status(device_id) {
             '> Altitude | ' + device.data.loc.alt + " m\n" +
             '> Heading | ' + device.data.loc.hd + "°\n" +
             '> Temp | ' + (Math.round(((device.data.loc.temp*(9/5))+32)*100)/100) + "°F\n" +
-            '> Last Heard | ' + moment(device.last_data_update).format('MM/DD/YY [at] h:mm:ss a') + "\n" +
+            '> Last Heard | ' + moment(device.last_data_update).format('h:mm:ss a [on] MM/DD/YY Z [UTC]') + "\n" +
             '> Alert Frequency | At most every ' + device.alert_freq_min + " mins\n" +
             ' http://maps.apple.com/maps?q=' + device.data.loc.lat + ',' + device.data.loc.lon + '\n';
     } else {
@@ -237,9 +255,9 @@ function get_status(device_id) {
 bot.on('error', err => {
     console.warn(err);
 });
-//End of Discord integration - - - - - - - - - - - - -
+//End of Discord integration - - - - - - - - - - - - - - - - - - - - - - - - -
 
-//API Webhooks - - - - - - - - - - - - - - - - - - - -
+//API Webhooks - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //Setup express
 const app = express();
 app.use(bodyParser.json());
@@ -249,74 +267,77 @@ app.post("/api/particle/trackerone", (req, res) => {
     spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: Received POST request, parsing data`);
     //Make sure we are authenticated
     if (req.body.api_key === config_storage.get('webhook_secret')) {
-        //Make sure trig is not from GPS lock
+        //Get device data
+        let device = devices_storage.get(req.body.coreid);
+        if (device === undefined) {
+            device = {};
+            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Creating new device in devices.json`);
+        }
+
+        //Make sure we have a GPS lock before updating GPS data
+        if ((JSON.parse(req.body.data).loc.lat === undefined || JSON.parse(req.body.data).loc.lon === undefined) && device !== {}) {
+            device.data.loc.cell = JSON.parse(req.body.data).loc.cell;
+            device.data.loc.batt = JSON.parse(req.body.data).loc.batt;
+            device.data.loc.temp = JSON.parse(req.body.data).loc.temp;
+            device.data.trig = JSON.parse(req.body.data).trig;
+            spinner.warn(`${chalk.yellow('API /api/particle/trackerone')}: Not updating device GPS data because there is no GPS lock`);
+        } else {
+            device.data = JSON.parse(req.body.data);
+        }
+
+        //Make sure device_name, alert_freq_min, last_alert_update exists for device
+        if (device.alert_freq_min === undefined) {
+            device.alert_freq_min = 60;
+            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "alert_freq_min" set to default: ` + device.alert_freq_min);
+        }
+        if (device.device_name === undefined) {
+            device.device_name = req.body.coreid;
+            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "device_name" set to default: ` + device.device_name);
+        }
+        if (device.last_alert_update === undefined) {
+            device.last_alert_update = moment();
+            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "last_alert_update" set to default: ` + device.last_alert_update);
+        }
+
+        //Determine trigger reason
         let check_reason = "false";
-        //Loop through trigger reasons
         for (let sent_reason of JSON.parse(req.body.data).trig) {
             if (sent_reason === "radius") {
-                check_reason = " has changed in GPS location at ";
+                check_reason = " has moved past its GPS radius threshold at ";
             } else if (sent_reason === "imu_m" || sent_reason === "img_g") {
-                check_reason = " has been moved physically at ";
-            } else if (sent_reason === "lock") {
-                check_reason = "lock";
+                check_reason = " has detected a physical motion alert at ";
             }
         }
-        if (check_reason !== "lock") {
-            //Get device data
-            let device = devices_storage.get(req.body.coreid);
-            if (device === undefined) {
-                device = {};
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Creating new device in devices.json`);
-            }
-            device.data = JSON.parse(req.body.data);
 
-            //Make sure device_name, alert_freq_min, last_alert_update exists for device
-            if (device.alert_freq_min === undefined) {
-                device.alert_freq_min = 60;
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "alert_freq_min" set to default: ` + device.alert_freq_min);
-            }
-            if (device.device_name === undefined) {
-                device.device_name = req.body.coreid;
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "device_name" set to default: ` + device.device_name);
-            }
-            if (device.last_alert_update === undefined) {
-                device.last_alert_update = moment();
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) "last_alert_update" set to default: ` + device.last_alert_update);
-            }
-
-            //If the trigger reason is valid, check to see if we can send an alert
-            if (check_reason !== "false") {
-                //Send an alert if the freq has passed
-                if (moment().isAfter(moment(device.last_alert_update).add(device.alert_freq_min, 'm'))) {
-                    device.last_alert_update = moment();
-                    spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event trigger reason is valid and is past the alert frequency, sending alert`);
-                    //Send message to Discord
-                    let message = '@everyone ' + device.device_name + check_reason + moment().format('MM/DD/YY h:mm:ss a');
-                    bot.createMessage(config_storage.get('discord_bot_channel'), message);
-                    spinner.info(`${chalk.cyan('Discord')}: ${chalk.yellow('API /api/particle/trackerone')} triggered message send to channel "` + message + `"`);
-                    //Prepare for status call on Discord
-                    last_alert_device_id = req.body.coreid;
-                } else {
-                    spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event trigger reason is valid but not past the alert frequency, skipping alert`);
-                }
+        //If the trigger reason is valid, check to see if we can send an alert
+        if (check_reason !== "false") {
+            //Send an alert if the freq has passed
+            if (moment().isAfter(moment(device.last_alert_update).add(device.alert_freq_min, 'm'))) {
+                device.last_alert_update = moment.unix(JSON.parse(req.body.data).time);
+                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event trigger reason is valid and is past the alert frequency, sending alert`);
+                //Send message to Discord
+                let message = '@everyone ' + device.device_name + check_reason + moment.unix(JSON.parse(req.body.data).time).format('h:mm:ss a [on] MM/DD/YY Z [UTC]');
+                bot.createMessage(config_storage.get('discord_bot_channel'), message);
+                spinner.info(`${chalk.cyan('Discord')}: ${chalk.yellow('API /api/particle/trackerone')} triggered message send to channel "` + message + `"`);
+                //Prepare for status call on Discord
+                last_alert_device_id = req.body.coreid;
             } else {
-                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event does not match a valid trigger reason, skipping alert`);
+                spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event trigger reason is valid but not past the alert frequency, skipping alert`);
             }
-
-            //Update device data
-            devices_storage.set(req.body.coreid, {
-                device_name: device.device_name,
-                data: device.data,
-                last_data_update: moment(req.body.published_at),
-                last_alert_update: device.last_alert_update,
-                alert_freq_min: device.alert_freq_min
-            });
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Updated data for device`);
-            res.status(200).end();
         } else {
-            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: Ignoring GPS lock trigger, not updating data`);
-            res.status(200).end();
+            spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Event does not match a valid trigger reason, skipping alert`);
         }
+
+        //Update device data
+        devices_storage.set(req.body.coreid, {
+            device_name: device.device_name,
+            data: device.data,
+            last_data_update: moment(req.body.published_at),
+            last_alert_update: device.last_alert_update,
+            alert_freq_min: device.alert_freq_min
+        });
+        spinner.info(`${chalk.yellow('API /api/particle/trackerone')}: (id:` + req.body.coreid + `) Updated data for device`);
+        res.status(200).end();
     } else {
         spinner.warn(`${chalk.yellow('API /api/particle/trackerone')}: Unauthorized POST request`);
         res.status(403).end();
@@ -332,4 +353,4 @@ app.listen(config_storage.get('api_port'), function () {
     spinner.start('Attempting to connect to Discord API');
     bot.connect();
 })
-//End of API Webhooks - - - - - - - - - - - - - - - - -
+//End of API Webhooks - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
